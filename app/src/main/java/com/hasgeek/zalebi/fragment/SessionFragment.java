250f8c65619037d1c84637d8bd7329733a476fd4
leenasn <leena.sn@multunus.com>
2015-07-26 07:37:10 +0530

    Implement Sync adapter for syncing session details in the background

--- app/src/main/java/com/hasgeek/zalebi/fragment/SessionFragment.java
+++ app/src/main/java/com/hasgeek/zalebi/fragment/SessionFragment.java
@@ -1,10 +1,15 @@
 package com.hasgeek.zalebi.fragment;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +24,16 @@ import java.util.ArrayList;
 public class SessionFragment extends Fragment implements SessionsReader.SessionReadListener{
 
     RecyclerView mRecyclerView;
+    // The authority for the sync adapter's content provider
+    public static final String AUTHORITY = "com.hasgeek.zalebi.sync.provider";
+    // An account type, in the form of a domain name
+    public static final String ACCOUNT_TYPE = "com.hasgeek.zalebi.account";
+    // The account name
+    public static final String ACCOUNT = "dummyaccount";
+
+    // Sync interval constants
+    public static final long SYNC_INTERVAL = 120L;
+    private Account mAccount;
 
     public SessionFragment() {
     }
@@ -26,7 +41,6 @@ public class SessionFragment extends Fragment implements SessionsReader.SessionR
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        new SessionFetcher(getActivity().getApplicationContext(), this).fetch();
 
     }
 
@@ -37,6 +51,21 @@ public class SessionFragment extends Fragment implements SessionsReader.SessionR
         //mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(mRecyclerView.getContext()));
         new SessionsReader(getActivity().getApplicationContext(),this).readSessions();
+        mAccount = createSyncAccount(mRecyclerView.getContext());
+        ContentResolver.setSyncAutomatically(mAccount, AUTHORITY, true);
+        ContentResolver.setIsSyncable(mAccount, AUTHORITY, 1);
+
+        ContentResolver.addPeriodicSync(
+                mAccount,
+                AUTHORITY,
+                Bundle.EMPTY,
+                SYNC_INTERVAL);
+        Log.d("hasgeek", "initiated periodic sync for SyncAdapter");
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        ContentResolver.requestSync(mAccount, AUTHORITY, bundle);
+        Log.d("hasgeek", "initiated requestSync for immeidate sync with SyncAdapter");
         return mRecyclerView;
     }
 
@@ -60,4 +89,22 @@ public class SessionFragment extends Fragment implements SessionsReader.SessionR
     public void onSessionReadFailure() {
 
     }
+
+    /** Create a new dummy account for the sync adapter
+     *
+     * @param context The application context
+     */
+    public static Account createSyncAccount(Context context) {
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        Context.ACCOUNT_SERVICE);
+        // Create the account type and default account
+        Account newAccount = new Account(
+                ACCOUNT, ACCOUNT_TYPE);
+        accountManager.addAccountExplicitly(newAccount,null,null);
+        return newAccount;
+    }
+
+
 }
--- app/src/main/java/com/hasgeek/zalebi/network/SessionFetcher.java
+++ app/src/main/java/com/hasgeek/zalebi/network/SessionFetcher.java
@@ -1,6 +1,7 @@
 package com.hasgeek.zalebi.network;
 
 import android.content.Context;
+import android.os.Environment;
 import android.util.Log;
 
 import com.android.volley.Request;
@@ -10,15 +11,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.hasgeek.zalebi.model.Session;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileWriter;
 
 
 public class SessionFetcher {
@@ -35,20 +33,14 @@ public class SessionFetcher {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    ArrayList<Session> sessionList = new ArrayList<>();
-                    Gson gson = new Gson();
-                    JSONArray schedules = response.getJSONArray("schedule");
-                    for(int i=0; i<schedules.length(); i++){
-                        JSONObject schedule = (JSONObject) schedules.get(i);
-                        JSONArray slots = schedule.getJSONArray("slots");
-                        //sessionList.addAll(Arrays.asList(gson.fromJson(slots.getJSONArray("sessions").toString(), Session[].class)));
-                        for(int j = 0; j<slots.length(); j++){
-                            JSONObject slot = (JSONObject) slots.get(j);
-                            sessionList.addAll(Arrays.asList(gson.fromJson(slot.getString("sessions"), Session[].class)));
-                            Log.d("talkfunnel","added sessions for slot "+slot.getString("slot"));
-                        }
-                    }
-                } catch (JSONException e) {
+                    File dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/hasgeek");
+                    dir.mkdir();
+                    FileWriter fileWriter = new FileWriter(new File(dir.getAbsolutePath() + "/sessions.json"));
+                    Log.d("hasgeek","Written the sessions JSON successfully*****");
+                    fileWriter.write(response.toString());
+                    fileWriter.close();
+                } catch (Exception e) {
+                    Log.e("hasgeek","Exception raised!!!!"+e.getMessage());
                     e.printStackTrace();
                 }
             }
@@ -61,10 +53,4 @@ public class SessionFetcher {
 
         requestQueue.add(jsonObjectRequest);
     }
-
-    private Session constructSession(JSONObject jsonObject){
-        Gson gson = new Gson();
-        return gson.fromJson(jsonObject.toString(), Session.class);
-    }
-
 }
