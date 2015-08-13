36be4c9245a84c7861f446ed5391eaa2c39b37d5
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2015-08-13 21:39:50 +0530

    UX changes for session details page
     - Implement collapsing toolbar
     - Add dummy cover photo and floating icon

--- app/src/main/java/com/hasgeek/zalebi/activity/SessionActivity.java
+++ app/src/main/java/com/hasgeek/zalebi/activity/SessionActivity.java
@@ -1,17 +1,21 @@
 package com.hasgeek.zalebi.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.hasgeek.zalebi.R;
 import com.hasgeek.zalebi.model.Session;
 import com.hasgeek.zalebi.util.DateTimeUtils;
 
-public class SessionActivity extends ActionBarActivity {
+public class SessionActivity extends AppCompatActivity {
     Session mSession;
 
     @Override
@@ -19,7 +23,23 @@ public class SessionActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session);
         mSession = getSessionDetailsFromBundle();
-   }
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(mSession.getTitle());
+        collapsingToolbar.setExpandedTitleTextAppearance(R.style.expandedTitle);
+
+        loadBackdrop();
+    }
+
+    private void loadBackdrop() {
+        final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
+        Glide.with(this).load(R.drawable.bb).centerCrop().into(imageView);
+    }
 
     @Override
     protected void onResume() {
@@ -29,7 +49,7 @@ public class SessionActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_session, menu);
+        //getMenuInflater().inflate(R.menu.menu_session, menu);
         return true;
     }
 
@@ -50,7 +70,7 @@ public class SessionActivity extends ActionBarActivity {
     }
 
     private void setSessionDetails() {
-        setSessionTextView(R.id.session_title, mSession.getTitle());
+        //setSessionTextView(R.id.session_title, mSession.getTitle());
         setSessionTextView(R.id.session_day, DateTimeUtils.displayableDate(mSession.getStart()));
         setSessionTextView(R.id.session_time_interval,
                 DateTimeUtils.displayableTimeInterval(mSession.getStart(), mSession.getEnd()));
