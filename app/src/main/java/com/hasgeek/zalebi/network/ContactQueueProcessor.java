b72b8adfd609ce9fa58d12ef6a8c5ae41975ac37
Vimal V Nair <vimalvnair999@gmail.com>
2015-08-16 16:55:32 +0530

    add ContactQueueProcessor

---
+++ app/src/main/java/com/hasgeek/zalebi/network/ContactQueueProcessor.java
@@ -0,0 +1,43 @@
+package com.hasgeek.zalebi.network;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.hasgeek.zalebi.model.Contact;
+import com.hasgeek.zalebi.model.ContactQueue;
+
+import java.util.List;
+
+/**
+ * Created by heisenberg on 16/08/15.
+ */
+public class ContactQueueProcessor implements ContactFetcher.ContactFetchListener {
+    Context mContext;
+    ContactFetcher mContactFetcher;
+
+    public ContactQueueProcessor(Context mContext) {
+        this.mContext = mContext;
+        mContactFetcher = new ContactFetcher(mContext, this);
+    }
+
+    public void process() {
+        List<ContactQueue> contactQueues = ContactQueue.listAll(ContactQueue.class);
+        for (ContactQueue contactQueue : contactQueues) {
+            Log.e("funnel", "fetching for " + contactQueue.getParticipantUrl());
+            mContactFetcher.fetch(contactQueue.getParticipantUrl());
+        }
+    }
+
+    @Override
+    public void onContactFetchSuccess(Contact contact) {
+        List<ContactQueue> contactQueues = ContactQueue.find(ContactQueue.class, "user_id", contact.getUserId());
+        for (ContactQueue contactQueue : contactQueues) {
+            contactQueue.delete();
+        }
+    }
+
+    @Override
+    public void onContactFetchFailure() {
+
+    }
+}
