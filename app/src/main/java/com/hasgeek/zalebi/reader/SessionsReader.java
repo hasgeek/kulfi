80ddb9688aa490194515b4f9c238e0740e418512
Vimal V Nair <vimalvnair999@gmail.com>
2015-09-11 17:04:25 +0530

    sort the sessions by start date

--- app/src/main/java/com/hasgeek/zalebi/reader/SessionsReader.java
+++ app/src/main/java/com/hasgeek/zalebi/reader/SessionsReader.java
@@ -8,8 +8,14 @@ import com.hasgeek.zalebi.activity.TalkFunnelActivity;
 import com.hasgeek.zalebi.model.Session;
 import com.hasgeek.zalebi.util.DateTimeUtils;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.TimeZone;
 
 public class SessionsReader {
 
@@ -25,6 +31,22 @@ public class SessionsReader {
         List<Session> sessions = new ArrayList<>();
         try {
             sessions = Session.find(Session.class,"space_id = ?", TalkFunnelActivity.SPACE_ID);
+            Collections.sort(sessions, new Comparator<Session>() {
+                @Override
+                public int compare(Session lhs, Session rhs) {
+
+                    DateFormat m_ISO8601Local = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                    m_ISO8601Local.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+                    try {
+                        return m_ISO8601Local.parse(lhs.getStart()).compareTo(m_ISO8601Local.parse(rhs.getStart()));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    return 0;
+                }
+            });
+
             listener.onSessionReadSuccess(addHeadersToSessions(sessions));
         }
         catch (Exception ex){
